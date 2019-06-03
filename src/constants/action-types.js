8b844fae84cda28a49af208ba7feef03cda6577d
teenoh <ugbalaachinike@gmail.com>
2019-06-03 12:09:09 +0100

    add action types

---
+++ src/constants/action-types.js
@@ -0,0 +1,5 @@
+export const SET_ACTIVE_USER_ID = 'SET_ACTIVE_USER_ID';
+export const SET_TYPING_VALUE = 'SET_TYPING_VALUE';
+export const SEND_MESSAGE = 'SEND_MESSAGE';
+export const EDIT_MESSAGE = 'EDIT_MESSAGE';
+export const DELETE_MESSAGE = 'DELET_MESSAGE';
